1596e4b9f79e287c9dca503874846f6780b99af6
MBakac <martin.bakac@gmail.com>
2023-05-19 14:10:47 +0200

    decompression

--- src/SCCGD.cpp
+++ src/SCCGD.cpp
@@ -14,6 +14,18 @@ void unzipFile(std::string filePath, std::string outputFilePath) {
 	system(("7za e " + filePath + " -o" + outputFilePath + " -aos").c_str());
 }
 
+
+/* COPY PASTE s https://www.delftstack.com/howto/cpp/read-file-into-string-cpp/ */
+std::string readFileIntoString(const std::string& path) {
+    std::ifstream input_file(path);
+    if (!input_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
 int main( int argc, char **argv){
 	if (argc <= 3) {
         std::cerr << "Missing argument." << std::endl;
@@ -25,6 +37,10 @@ int main( int argc, char **argv){
     std::string finalFolder = argv[3];
 
     unzipFile(compressedFilePath, "../data/resultsd/decompressed");
+    std::string filename("../data/resultsd/decompressed/intermediate.txt");
+
+    std::string compressedFile = readFileIntoString(filename);
+	std::cout << compressedFile << std::endl;
 
 	return 0;
 }
